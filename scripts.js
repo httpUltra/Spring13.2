4e4ef442c857fe2bf0dbc0479f90420c6bbf62b0
Ultra Lord <haxutoasty@gmail.com>
2023-08-02 19:36:11 -0500

    Added first and second half of Find/FindIndex

--- scripts.js
+++ scripts.js
@@ -1,42 +1,61 @@
-// SOME ODDNUMBER
-function oddNums(arr) {
-    return arr.some(function(num) {
-        return num % 2 !== 0;
-    });
-}
+// // SOME ODDNUMBER
+// function oddNums(arr) {
+//     return arr.some(function(num) {
+//         return num % 2 !== 0;
+//     });
+// }
 
-// HAS A ZERO SOME
-function aZero(arr) {
-    return arr.some(value => String(value).includes('0'));
-}
+// // HAS A ZERO SOME
+// function aZero(arr) {
+//     return arr.some(value => String(value).includes('0'));
+// }
 
-// ONLYODD EVERY
-function onlyOdd(arr) {
-    return arr.every(num => {return num % 2 !== 0});
-}
+// // ONLYODD EVERY
+// function onlyOdd(arr) {
+//     return arr.every(num => {return num % 2 !== 0});
+// }
 
-// HAS NO DUPLICATES
-function noDupes(arr) {
-    return arr.every(value => {return arr.indexOf(value) === arr.lastIndexOf(value)});
-}
+// // HAS NO DUPLICATES
+// function noDupes(arr) {
+//     return arr.every(value => {return arr.indexOf(value) === arr.lastIndexOf(value)});
+// }
 
-// HAS CERTAIN KEY
-let arrOne = [
-    {title: "Instructor", first: 'Elie', last:"Schoppik"},
-    {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true},
-    {title: "Instructor", first: 'Matt', last:"Lane"},
-    {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
-  ]
+// // HAS CERTAIN KEY
+// let arrOne = [
+//     {title: "Instructor", first: 'Elie', last:"Schoppik"},
+//     {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true},
+//     {title: "Instructor", first: 'Matt', last:"Lane"},
+//     {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
+//   ]
 
-function hasKey(arr, key){
-    return arr.every(function(obj){
-        return obj[key];
-    });
-}
+// function hasKey(arr, key){
+//     return arr.every(function(obj){
+//         return obj[key];
+//     });
+// }
 
-// HAS CERTAIN VALUE
-function hasVal(arr, key, val){
-    return arr.every(function(obj){
-      return obj[key] === val;;
+// // HAS CERTAIN VALUE
+// function hasVal(arr, key, val){
+//     return arr.every(function(obj){
+//       return obj[key] === val;;
+//     })
+// }
+// FIND BY USERNAME
+const users = [
+    {username: 'mlewis'},
+    {username: 'akagen'},
+    {username: 'msmith'}
+];
+
+
+function byUsername(arr, search){
+    return arr.find(function (obj){
+        return obj.username === search;
     })
+}
+
+
+// REMOVE USER
+function removeUser(arr){
+    arr.find
 }
